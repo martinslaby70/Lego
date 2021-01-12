030efe26056ce28393dae1b110f18f1618b7df6e
Martin Slabý <martin.slaby29@seznam.cz>
2021-01-12 21:10:27 +0100

    added categories

--- server/scraping/scraping.js
+++ server/scraping/scraping.js
@@ -2,18 +2,19 @@ const puppeteer = require('puppeteer');
 
 
 const getAlzaItems = async () => {
-    let Url = 'https://www.alza.cz/hracky/nejprodavanejsi-nejlepsi-lego/18851136.htm#f&cst=null&cud=0&pg=1-999prod=&sc=2500';
+    let Url = 'https://www.alza.cz/hracky/nejprodavanejsi-nejlepsi-lego/18851136.htm#f&cst=null&cud=0&pg=0-99prod=&sc=2500';
 
     let browser = await puppeteer.launch({headless: true});
     let page = await browser.newPage();
-
+    
     await page.goto(Url, { waitUntil: 'networkidle2' });   
    
     const items = await page.$$('.box.browsingitem');
     const itemsCount = items.length;
+    let salesCount = 0;
     let itemsInSale = [];
     
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 0; i < itemsCount; i++) {
         const item = items[i];
         
         //replace with condition
@@ -26,6 +27,9 @@ const getAlzaItems = async () => {
             const description = await (await item.$eval('.Description', span => span.textContent)).trim('\n');
             const link = await item.$eval('a.name', a => 'https://www.alza.cz' + a.getAttribute('href'));
             const img = await item.$eval('em > img', img => img.getAttribute('data-src')); 
+            const categories = await item.$eval('a.pc.browsinglink', a => a.getAttribute('data-impression-category').replace(/\s/g, '').split("\\").filter(category => category !== 'Hračky').filter(category => category !== 'LEGO'));
+            
+            salesCount++;
 
             itemsInSale.push({
                 name,
@@ -34,11 +38,12 @@ const getAlzaItems = async () => {
                 sale,
                 description,
                 link,
-                img
+                img,
+                categories
             })
         }
-        catch {
-
+        catch(err){
+            //console.log(err);
         }
         
     }
@@ -47,10 +52,17 @@ const getAlzaItems = async () => {
 
     return {
         itemsCount,
+        salesCount,
         itemsInSale
     };
 }
 
+const getCzcItems = async () => {
 
 
-module.exports.getAlzaItems = getAlzaItems;
+
+}
+
+
+
+module.exports = {getAlzaItems};
