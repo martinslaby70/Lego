e9a5457c31e63ba7e04ccde4164abd5cda4be219
Martin Slabý <martin.slaby29@seznam.cz>
2021-01-13 10:40:24 +0100

    czc scraping

--- server/scraping/scraping.js
+++ server/scraping/scraping.js
@@ -24,7 +24,7 @@ const getAlzaItems = async () => {
             const originalPrice = await item.$eval('span.np2', span => span.textContent);
             const currentPrice = await item.$eval('span.c2', span => span.textContent);
             const sale = await item.$eval('span.np', span => span.textContent);
-            const description = await (await item.$eval('.Description', span => span.textContent)).trim('\n');
+            const description = await (await item.$eval('.Description', span => span.textContent)).trim('\\n');
             const link = await item.$eval('a.name', a => 'https://www.alza.cz' + a.getAttribute('href'));
             const img = await item.$eval('em > img', img => img.getAttribute('data-src')); 
             
@@ -53,6 +53,7 @@ const getAlzaItems = async () => {
             })
         }
         catch(err){
+            //not in sale
             //console.log(err);
         }
         
@@ -69,56 +70,86 @@ const getAlzaItems = async () => {
 
 
 const getCzcItems = async () => {
-    let Url = 'https://www.czc.cz/lego/lego/zbozi?q-first=0';
+        const Url = 'https://www.czc.cz/lego/lego/zbozi?q-first=0';
 
-    let browser = await puppeteer.launch({headless: false});
-    let page = await browser.newPage();
-    
-    await page.goto(Url, { waitUntil: 'networkidle2' });   
-   
-    
-    const pagesCount = await page.$eval('.paging.ajax > a.last', a => Number(a.innerHTML))
-    
-    let itemsInSale = [];
-    let salesCount = 0;
+        let browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--incognito',
+            ]
+        });
+        let page = await browser.newPage();
 
+        await page.goto(Url, { waitUntil: 'networkidle0' });   
 
-    for (let i = 0; i < pagesCount - 1; i++) {
-        const button = await page.$('button.btn.show-next');
-        button.click();    
-    }
-
-    await page.waitForSelector('#titles');
-
-    const items = await page.$$('#titles > div > div');
-    const itemsCount = items.length;
-
-    for (const item of items) {
-      
+        let itemsInSale = [];
+        let salesCount = 0;
+        const itemsCount = await page.$eval('.order-by-sum.h-800', div => parseInt(div.textContent)); 
+        const pagesCount = await page.$eval('.paging.ajax > a.last', a => a.textContent)
         
-        try {
-            const name = item.$eval('.tile-title > h5 > a', a => a.textContent);
-            const img = item.$eval('a.image > img', img => img.getAttribute('src'));
+        for (let i = 0; i < pagesCount-1; i++) {
+           
+            const items = await page.$$('.new-tile');
 
-            console.log({name, img});
+            for (const item of items) {
+               try {
+                    const name = await item.$eval('.tile-title > h5 > a', a => a.textContent.trim('\\n'));
+                    const img = await item.$eval('.img-wrapper a img', img => img.getAttribute('src'));   
+                    const originalPrice = await item.$eval('span.price-before > span.price-vatin', span => span.textContent);
+                    const currentPrice = await item.$eval('span.price > span.price-vatin', span => span.textContent);
+                    const sale = await item.$eval('.img-wrapper > span.discount-percent', span => span.textContent.trim('\\n'));
+                    const description = await item.$eval('.tile-desc', span => span.textContent.trim('\\n'));
+                    const link = await item.$eval('a.image', a => 'https://www.czc.cz' + a.getAttribute('href'));
+                    
+                    const avalibleCategories = ['City', 'Technik', 'Exlusive', 'Creator', 'Ninjago', 'Star Wars', 'Architecture', 'Super Mario', 'Harry Potter', 'Marvel'];
+                    let categories = [];
+                    for (let i = 0; i < avalibleCategories.length; i++) {
+                        const category = avalibleCategories[i];
+                        if (description.toUpperCase().includes(category.toUpperCase()))
+                            categories.push(category);
+                    }
+                    
+                   
+                    salesCount++;
 
-            salesCount++;
-            itemsInSale.push({name,img})
+                    itemsInSale.push({
+                        name,
+                        originalPrice,
+                        currentPrice,
+                        sale,
+                        description,
+                        link,
+                        img,
+                        categories
+                    })   
+               } catch (err) {
+                   //not in sale
+                   //console.log(err);
+               }
+            }
+            
+            try {
+                const button = await page.$('.paging.ajax > a.page-next');
+                await Promise.all([
+                    button.click(),
+                    page.waitForNavigation()
+                ]);
+            } catch (error) {
+                console.log(error);
+            }
 
-        } catch (err) {
-            console.log(err);
+           
         }
-    }
 
-    await browser.close();
-
-    return {
+    browser.close();   
+    return({
         itemsCount,
         salesCount,
         itemsInSale
-    };
+    });
 
-
+   
+    
 }
 
 
--- server/test.js
+++ server/test.js
@@ -4,25 +4,76 @@ const puppeteer = require('puppeteer');
     try{
         let Url = 'https://www.czc.cz/lego/lego/zbozi?q-first=0';
 
-        let browser = await puppeteer.launch({headless: false});
+        let browser = await puppeteer.launch({headless: true});
         let page = await browser.newPage();
 
-        await page.goto(Url, { waitUntil: 'networkidle2' });   
+        await page.goto(Url, { waitUntil: 'networkidle0' });   
 
 
         const pagesCount = await page.$eval('.paging.ajax > a.last', a => a.textContent)
 
         let itemsInSale = [];
         let salesCount = 0;
-
+        const itemsCount = await page.$eval('.order-by-sum.h-800', div => parseInt(div.textContent)); 
         console.log(pagesCount);
         
 
-        const button = await page.$('button.btn.show-next');
+        
         
         for (let i = 0; i < pagesCount-1; i++) {
-            await button.click();
-            await page.waitFor(1000);
+           
+            console.log('\n\n\nLOADED\n-------------------------------------------------');
+
+            const items = await page.$$('.new-tile');
+
+            for (const item of items) {
+                
+                const name = item.$eval('.tile-title > h5 > a', a => a.textContent);
+                const img = item.$eval('a.image > img', img => img.getAttribute('src'));   
+
+                const originalPrice = await item.$eval('span.price-before > span.price-vatin', span => span.textContent);
+                const currentPrice = await item.$eval('span.price > span.price-vatin', span => span.textContent);
+                const sale = await item.$eval('.img-wrapper > span.discount-percent', span => span.textContent);
+                const description = await item.$eval('.tile-desc', span => span.textContent);
+                const link = await item.$eval('aimage', a => 'https://www.czc.cz' + a.getAttribute('href'));
+                
+                
+                const categories = () => {
+                    const avalibleCategories = ['City', 'Technik', 'Exlusive', 'Creator', 'Ninjago', 'Star Wars', 'Architecture', 'Super Mario', 'Harry Potter', 'Marvel'];
+                    let returnCategories = [];
+
+                    avalibleCategories.forEach(category => {
+                        if (description.toUpperCase().trim().includes(category.trim().toUpperCase()))
+                            returnCategories.push(category);
+                    })
+                   
+
+                    return returnCategories
+                }
+
+                itemsInSale.push({
+                    name,
+                    originalPrice,
+                    currentPrice,
+                    sale,
+                    description,
+                    link,
+                    img,
+                    categories
+                })   
+            }
+            
+            try {
+                const button = await page.$('.paging.ajax > a.page-next');
+                await Promise.all([
+                    button.click(),
+                    page.waitForNavigation()
+                ]);
+            } catch (error) {
+                console.log(error);
+            }
+
+           
         }
        
         
@@ -52,10 +103,14 @@ const puppeteer = require('puppeteer');
        
        
 
-        
+    console.log({
+        itemsCount,
+        salesCount,
+        itemsInSale
+    });
 
-       
-
+    browser.close();   
+    
     } catch (err) {
         console.log(err);
     }
